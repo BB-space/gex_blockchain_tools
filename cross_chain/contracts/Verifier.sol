d0fa3de6b181d769d6c730da63ebca8a43930d13
MKateryna Khotkevych <kkhotkevych@gmail.com>
2017-10-17 12:11:36 +0300

    temp

--- cross_chain/contracts/Verifier.sol
+++ cross_chain/contracts/Verifier.sol
@@ -2,7 +2,12 @@ pragma solidity ^0.4.8;
 
 
 contract Verifier {
+
+
+    event Verify(bytes32 b);
+
     function recoverAddr(bytes32 msgHash, uint8 v, bytes32 r, bytes32 s) returns (address) {
+        Verify(msgHash);
         return ecrecover(msgHash, v, r, s);
     }
 
