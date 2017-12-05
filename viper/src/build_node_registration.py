7f84e5888d6448d5e6548480c0a74181833c3ca9
MKateryna Khotkevych <kkhotkevych@gmail.com>
2017-12-05 15:55:52 +0200

    cleaning

--- viper/src/build_node_registration.py
+++ viper/src/build_node_registration.py
@@ -21,16 +21,16 @@ TOKEN_SYMBOL = "FANG"
 TOKEN_DECIMALS = 18
 TOKEN_INITIAL_SUPPLY = 21000000
 TOKEN_TOTAL_SUPPLY = 21000000000
-#token_contract_address, token_contract_abi = deployer.deploy("contracts/vipercoin.v.py",
- #                                                            (TOKEN_NAME, TOKEN_SYMBOL, TOKEN_DECIMALS,
-  #                                                            TOKEN_INITIAL_SUPPLY))
+token_contract_address, token_contract_abi = deployer.deploy("contracts/vipercoin.v.py",
+                                                             (TOKEN_NAME, TOKEN_SYMBOL, TOKEN_DECIMALS,
+                                                              TOKEN_INITIAL_SUPPLY))
 
-registration_contract_address, registration_contract_abi = deployer.deploy("contracts/test.v.py", None)
-                                                                           #token_contract_address) "contracts/node_registration.v.py"
+registration_contract_address, registration_contract_abi = deployer.deploy("contracts/node_registration.v.py",
+                                                                           token_contract_address)
 
 data = {}
-#data['token_address'] = token_contract_address
-#data['token_abi'] = token_contract_abi
+data['token_address'] = token_contract_address
+data['token_abi'] = token_contract_abi
 data['registration_address'] = registration_contract_address
 data['registration_abi'] = registration_contract_abi
 write_to_file(data, "../data.json")
