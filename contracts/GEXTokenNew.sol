43fcf7f81a7698ca0e97986ec84f53d02d8d31f9
Artem <stan@galacticexchange.io>
2018-01-11 19:38:20 +0200

    Added a new GEXToken withan embedded GexBot. Test!

---
+++ contracts/GEXTokenNew.sol
@@ -0,0 +1,47 @@
+pragma solidity ^0.4.15;
+
+
+import './StandardToken.sol';
+import './Ownable.sol';
+import './GexBotNew.sol';
+
+
+contract GEXTokenNew is GexBotNew {
+
+    //Token values
+    string public constant name = "Galactic Exchange Token";
+
+    string public constant symbol = "GEX";
+
+    uint public constant decimals = 18;
+
+    uint public constant cap = 100000 * 10 ** decimals; // the maximum amount of tokens that can ever be created
+
+    event Mint(address indexed to, uint256 amount);
+
+    event Burn(address indexed from, uint256 amount);
+
+    function GEXTokenNew() GexBotNew() {
+        balances[msg.sender] = 1000 * 10 ** decimals;
+        // TODO remove after testing
+        previousTime = uint40(block.timestamp);
+        previousEth = 1000000000000000000000;
+        previousGex = 1000000000000000000000;
+    }
+
+    function mint(address _to, uint256 _amount) onlyOwner returns (bool) {
+        require(totalSupply + _amount <= cap);
+        totalSupply = totalSupply + _amount;
+        balances[_to] = balances[_to] + _amount;
+        Mint(_to, _amount);
+        return true;
+    }
+
+    function burn(address _from, uint256 _amount) onlyOwner returns (bool) {
+        require(balances[_from] >= _amount);
+        balances[_from] = balances[_from] - _amount;
+        totalSupply = totalSupply - _amount;
+        Burn(_from, _amount);
+        return true;
+    }
+}
---
+++ contracts/GexBotNew.sol
@@ -0,0 +1,333 @@
+pragma solidity ^0.4.16;
+
+/*interface Token {
+    function transfer2(
+        address _to,
+        uint256 _value
+    )
+        public
+        returns (bool success);
+    function balanceOf(address _owner) public constant returns (uint balance);
+}*/
+
+import "./StandardToken.sol";
+import "./Ownable.sol";
+
+contract GexBotNew is StandardToken, Ownable{
+    struct Cell {
+        bool active;
+        uint88 amountEth;
+        uint88 amountGex;
+        uint88 loanEth;
+        uint88 loanGex;
+        int88 sendEth;
+        int88 sendGex;
+    }
+
+    uint88 public gexInitialReserve;
+    uint88 public ethInitialReserve;
+    uint40 public numberOfAddresses;
+    uint88 public currentGex;
+    uint88 public currentEth;
+    uint88 public previousGex;
+    uint88 public previousEth;
+    uint40 public previousTime;
+    uint24 constant public DAY = 300;
+
+    mapping (address => Cell) public applications;
+    mapping (uint40 => address) public addresses;
+
+    event Fee(uint88 value);
+    event ExchangedRateOfDay(
+        uint88 exchangeRateUp,
+        uint88 exchangeRateDown
+    );
+    event Message(
+        string inf,
+        address sender,
+        uint88 value
+    );
+
+    function GexBotNew() public {
+        previousEth = 1000000000000000000000;
+        previousGex = 1000000000000000000000;
+        previousTime = uint40(block.timestamp);
+    }
+
+    //Fallback function
+    function () public payable { putEth(""); }
+
+    //Fallback function ERC-223
+    /*function tokenFallback(
+        address _sender,
+        uint256 _value,
+        bytes _data
+    )
+        public
+    {
+        require(msg.sender == gexAddress);
+        require(_value >= 0);
+        if (_data.length == 0) {
+            putGex(_sender, uint88(_value));
+        } else {
+            bytes26 flag;
+            bytes26 addReserveKey = bytes26("addgalacticexchangereserve");
+            assembly {
+                flag := mload(add(_data, 0x20))
+            }
+            if (_sender == owner && flag == addReserveKey) {
+                gexInitialReserve += uint88(_value);
+            } else {
+                putGex(_sender, uint88(_value));
+            }
+        }
+    }*/
+
+    //Public functions
+    function putEth(bytes _data) public payable {
+        require(previousTime + DAY > block.timestamp);
+        if (_data.length != 0) {
+            bytes26 flag;
+            bytes26 addReserveKey = bytes26("addgalacticexchangereserve");
+            assembly {
+                flag := mload(add(_data, 0x20))
+            }
+            if (flag == addReserveKey && msg.sender == owner) {
+                ethInitialReserve += uint88(msg.value);
+                return;
+            }
+        }
+        uint88 amountEth = uint88(msg.value);
+        uint88 debt = uint88(-applications[msg.sender].sendEth);
+        if (debt > 0) {
+            if (amountEth >= debt) {
+                amountEth -= debt;
+                applications[msg.sender].sendEth = 0;
+            } else {
+                applications[msg.sender].sendEth += int88(amountEth);
+                amountEth = 0;
+            }
+            Message(
+                "Debt paid in eth",
+                msg.sender,
+                uint88(msg.value - amountEth)
+            );
+        }
+        if (amountEth > 0) {
+            uint40 index = numberOfAddresses;
+            if (!applications[msg.sender].active) {
+                applications[msg.sender].active = true;
+                addresses[index] = msg.sender;
+                numberOfAddresses++;
+            }
+            amountEth -= fee(amountEth, true); //up to 30 000 gas
+            applications[msg.sender].amountEth += amountEth;
+            currentEth += uint88(amountEth);
+            uint88 amount = uint88((uint256((amountEth * 80) / 100) *
+                uint256(previousGex + currentGex)) /
+                uint256(previousEth + currentEth));
+            applications[msg.sender].loanGex += amount;
+            transferGex(
+                msg.sender,
+                amount,
+                "Order is accepted and loan is sent in gex"
+            );
+        } else {
+            Message("Order is not accepted", msg.sender, 0);
+        }
+    }
+
+    function returnGex() public {
+        require(applications[msg.sender].sendGex > 0);
+        uint88 amount = uint88(applications[msg.sender].sendGex);
+        applications[msg.sender].sendGex = 0;
+        transferGex(msg.sender, amount, "Send gex successfully");
+    }
+
+    function returnEth() public {
+        require(applications[msg.sender].sendEth > 0);
+        uint88 amount = uint88(applications[msg.sender].sendEth);
+        applications[msg.sender].sendEth = 0;
+        transferEth(msg.sender, amount, "Send eth successfully");
+    }
+
+    function timeToChange() public { // about 23 000 gas
+        require(previousTime + DAY < block.timestamp);
+        require(msg.sender == owner);
+        uint88 totalEth = currentEth;
+        uint88 totalGex = currentGex;
+        if (numberOfAddresses > 0) {
+            uint40 index = numberOfAddresses;
+            address account;
+            Cell storage cell = applications[addresses[0]];
+            uint40 max = 0;
+            if (index > 10) {
+                max = index - 10;
+            }
+            for (int40 step = int40(index) - 1; step >= int40(max); step--) {
+                account = addresses[uint40(step)];
+                cell = applications[account];
+                if (totalGex > 0) {
+                    applications[account].sendEth +=
+                        int88(((int256(cell.amountGex) * int256(totalEth)) /
+                        int256(totalGex)) - int256(cell.loanEth));
+                }
+                applications[account].amountGex = 0;
+                applications[account].loanEth = 0;
+                if (totalEth > 0) {
+                    applications[account].sendGex +=
+                        int88(((int256(cell.amountEth) * int256(totalGex)) /
+                        int256(totalEth)) - int256(cell.loanGex));
+                }
+                applications[account].amountEth = 0;
+                applications[account].loanGex = 0;
+                applications[account].active = false;
+                delete(addresses[uint40(step)]);
+            }
+            numberOfAddresses -= index - max;
+        }
+        if (numberOfAddresses == 0) {
+            previousTime = uint40(block.timestamp);
+            previousEth = totalEth;
+            previousGex = totalGex;
+            ExchangedRateOfDay(totalEth, totalGex);
+            currentGex = 0;
+            currentEth = 0;
+        }
+        transferEth(
+            msg.sender,
+            uint88(msg.gas * tx.gasprice),
+            "Send money back"
+        );
+    }
+
+    //Private functions
+    function putGex(bytes _data, uint88 _amount) public {
+        require(previousTime + DAY > block.timestamp);
+        if (_data.length != 0) {
+            bytes26 flag;
+            bytes26 addReserveKey = bytes26("addgalacticexchangereserve");
+            assembly {
+                flag := mload(add(_data, 0x20))
+            }
+            if (flag == addReserveKey && msg.sender == owner) {
+                ethInitialReserve += uint88(msg.value);
+                return;
+            }
+        }
+        uint88 amountGex = _amount;
+        uint88 debt = uint88(-applications[msg.sender].sendGex);
+        if (debt > 0) {
+            if (_amount >= debt) {
+                amountGex -= debt;
+                applications[msg.sender].sendGex = 0;
+            } else {
+                amountGex = 0;
+                applications[msg.sender].sendGex += int88(_amount);
+            }
+            Message("Debt paid in gex", msg.sender, _amount - amountGex);
+        }
+        if (amountGex > 0) {
+            uint40 index = numberOfAddresses;
+            if (!applications[msg.sender].active) {
+                applications[msg.sender].active = true;
+                addresses[index] = msg.sender;
+                numberOfAddresses++;
+            }
+            amountGex -= fee(amountGex, false);  //up to 30 000 gas
+            applications[msg.sender].amountGex += uint88(amountGex);
+            currentGex += uint88(amountGex);
+            uint88 amount = uint88((uint256((amountGex * 80) / 100) *
+                uint256(previousEth + currentEth)) /
+                uint256(previousGex + currentGex));
+            applications[msg.sender].loanEth += uint88(amount);
+            transferEth(
+                msg.sender,
+                amount,
+                "Order is accepted and loan is sent in eth"
+            );
+        } else {
+            Message("Order is not accepted", msg.sender, 0);
+        }
+    }
+
+    function fee(       //up to 30 000 gas
+        uint88 _amount,
+        bool _ethCur
+    )
+        public
+        returns (uint88)
+    {
+        uint88 amountToOrder = _amount;
+        uint88 amountToOrderPrevious;
+        uint88 reserve;
+        uint88 up;
+        uint88 down;
+        uint88 initialReserve;
+        uint88 stateReserve;
+        if (_ethCur) {
+            up = previousGex + currentGex;
+            down = previousEth + currentEth;
+            initialReserve = gexInitialReserve;
+            stateReserve = uint88(balanceOf(address(this)));
+        } else {
+            up = previousEth + currentEth;
+            down = previousGex + currentGex;
+            initialReserve = ethInitialReserve;
+            stateReserve = uint88(this.balance);
+        }
+        for (uint8 step = 0; step < 20; step++) {
+            amountToOrderPrevious = amountToOrder;
+            reserve = stateReserve -
+                uint88((uint256(amountToOrderPrevious * 80) / 100 *
+                uint256(up)) / uint256(down + amountToOrderPrevious));
+            if (reserve >= initialReserve) {
+                amountToOrder = _amount;
+            } else {
+                amountToOrder = uint88((uint256(_amount) * 4 *
+                    uint256(reserve) * uint256(reserve)) /
+                    (uint256(initialReserve) *
+                    uint256(initialReserve - reserve) +
+                    4 * uint256(reserve) * uint256(reserve)));
+            }
+            if (amountToOrderPrevious == amountToOrder) {
+                break;
+            }
+        }
+        reserve = stateReserve -
+                uint88((uint256(amountToOrder * 80) / 100 *
+                uint256(up)) / uint256(down + amountToOrder));
+        if (reserve >= initialReserve) {
+            amountToOrder = 0;
+        } else {
+            amountToOrder = uint88((uint256(initialReserve) *
+                uint256(initialReserve - reserve) *
+                uint256(amountToOrder)) /
+                (4 * uint256(reserve) * uint256(reserve)));
+        }
+        Fee(amountToOrder);
+        return amountToOrder;
+    }
+
+    function transferGex(       //up to 42 000 gas
+        address _sender,
+        uint88 _amount,
+        string _message
+    )
+        public
+    {
+        Message(_message, _sender, _amount);
+        require(transfer(_sender, _amount));
+    }
+
+    function transferEth(       //up to 35 000 gas
+        address _sender,
+        uint88 _amount,
+        string _message
+    )
+        public
+    {
+        Message(_message, _sender, _amount);
+        _sender.transfer(_amount);
+    }
+}
