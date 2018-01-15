99ec16b9b939b2baeff3697c0627c4b9e83de0c5
MKateryna Khotkevych <kkhotkevych@gmail.com>
2018-01-15 18:02:36 +0200

    node manager contract annotation part 1

--- contracts/NodeManager.sol
+++ contracts/NodeManager.sol
@@ -1,76 +1,76 @@
 pragma solidity ^0.4.15;
 
-
+/// @title Node Manager contract
 contract NodeManager {
 
-    address public tokenAddress;
+    enum NodeStatus {Active, Leaving, Left}
 
-    uint public constant depositValue = 100; // todo rewrite coins //todo change 100 * 10 ** 18
-    uint constant leavingPeriod = 5260000; // 2 month. 3 month will be 7890000
-    uint constant paymentPeriod = 2764800; // 32*60*60*24
+    uint constant secondsToDay = 86400; // utility variable for time conversion
+    // 60 days in seconds. Time period after which user can withdraw node deposit and leave the system
+    uint constant leavingPeriod = 5260000;
+    // min term in which node can be rewarded for it's contribution
+    uint constant paymentDays = 32;
+    // paymentDays days in seconds
+    uint constant paymentPeriod = paymentDays * secondsToDay;
+    // permitted number of days node may not send the heartbit during the paymentPeriod
+    uint constant absentDays = 2;
     uint constant heartbitUnit = 256;
     uint constant heartbitTotal = heartbitUnit * 2;
-    uint constant daysForPayment = 32;
-    uint constant absentDays = 2;
-    uint constant timestampToDate = 86400;  // 24*60*60
 
-    uint startEpoch;
-    uint256 annualMint;
-    uint256 dailyMint;
-
-    enum NodeStatus {Active, Leaving, Left} // todo another names
+    address tokenAddress; // address of the token contract
+    uint256 annualMint; // total reword for nodes per year
+    uint256 dailyMint; // daily reword for nodes
+    uint depositValue; // size of a deposit to add node to the system
 
     struct Node {
-        uint256 deposit;
+        uint256 deposit; // todo do we need this variable at all?
         bytes15 ip; // todo use another type
         uint16 port; // todo The port number is an unsigned 16-bit integer, so 65535. https://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers
-        uint depositDate;
-        uint leavingDate;
-        uint lastRewardDate;
-        uint256[2] heartbits;
+        uint depositDate; // todo do we need this variable at all?
+        uint leavingDate; // date when node was moved to the Leaving state
+        uint lastRewardDate; // date when node was rewarded last time
+        uint256[2] heartbits; // bitmap of heartbit signals for last 512 days
         NodeStatus status;
     }
 
     struct BasicChannel {
-        address owner;
-        uint256 storageBytes;
-        uint lifetime;
-        uint startTime;
-        uint maxNodes;
-        uint256 deposit;
+        address owner; // channel owner
+        uint256 storageBytes; // number of bytes this channel can store
+        uint lifetime;  // number seconds this channel will be considered as alive
+        uint startDate; // date of channel creation
+        uint maxNodes; // max number of nodes associated with this channel
+        uint256 deposit; // value of tokens associated with this channel
     }
 
     struct AggregationChannel {
-        address owner;
-        uint256 storageBytes;
-        uint lifetime;
-        uint startTime;
-        uint maxNodes;
-        uint256 deposit;
-        mapping (uint256 => bool) basicChannels;
-        uint nextAggregatedBasicChannelIndex;
+        address owner; // channel owner
+        uint256 storageBytes; // number of bytes this channel can store
+        uint lifetime;  // number seconds this channel will be considered as alive
+        uint startDate; // date of channel creation
+        uint maxNodes; // max number of nodes associated with this channel
+        uint256 deposit; // value of tokens associated with this channel
+        mapping (uint256 => bool) basicChannels; // basic channels aggregated by this channel
+        uint nextAggregatedBasicChannelIndex; // index to store next basic channel
     }
 
+    // mapping of node index to a node instance
     mapping (uint256 => Node) nodes;
-
+    // mapping of a basic channel index to a basic channel instance
     mapping (uint256 => BasicChannel) basicChannel;
-
+    // mapping of an aggregation channel index to a aggregation channel instance
     mapping (uint256 => AggregationChannel) aggregationChannel;
-
-    mapping (address => mapping (uint256 => bool)) nodeIndexes; // todo set struct here
-
+    // mapping of owner address to node indexes associated with it
+    mapping (address => mapping (uint256 => bool)) nodeIndexes; // todo set struct here. list?? do the same as with channels
+    // mapping of owner address to basic channel indexes associated with it
     mapping (address => uint256[]) basicChannelIndexes;
-
+    // mapping of owner address to aggregation channel indexes associated with it
     mapping (address => uint256[]) aggregationChannelIndexes;
-
+    // index to store next node
     uint256 nextNodeIndex;
-
+    // index to store next basic channel
     uint256 nextBasicChannelIndex;
-
+    // index to store next aggregation channel
     uint256 nextAggregationChannelIndex;
-
-
-    event Fallback();
 
     event NodeCreated(
         uint256 nodeId,
@@ -105,21 +105,25 @@ contract NodeManager {
         uint16 nonce
     );
 
+
     // todo owner
     function NodeManager(address token, uint256 mintValue) {
         tokenAddress = token;
-        startEpoch = block.timestamp;
         annualMint = mintValue;
         dailyMint = annualMint / 365; // todo rewrite
         // todo check
         nextNodeIndex = 1;
         nextBasicChannelIndex = 1;
         nextAggregationChannelIndex = 1;
+        depositValue = 100; // todo change 100 * 10 ** 18
+    }
+
+    function getTime() public returns (uint) {
+        return block.timestamp;
     }
 
     // use should call transfer to make a deposit
     function tokenFallback(address sender, uint256 value, bytes data) public {
-        Fallback();
         require(msg.sender == tokenAddress);
         require(value == depositValue);
         uint16 port;
@@ -172,7 +176,7 @@ contract NodeManager {
         basicChannel[nextBasicChannelIndex].storageBytes = storageBytes;
         basicChannel[nextBasicChannelIndex].lifetime = lifetime;
         basicChannel[nextBasicChannelIndex].maxNodes = maxNodes;
-        basicChannel[nextBasicChannelIndex].startTime = block.timestamp;
+        basicChannel[nextBasicChannelIndex].startDate = block.timestamp;
         basicChannel[nextBasicChannelIndex].deposit = deposit;
         basicChannelIndexes[msg.sender].push(nextBasicChannelIndex);
         BasicChannelCreated(nextBasicChannelIndex, msg.sender, storageBytes, lifetime, maxNodes, nonce);
@@ -193,7 +197,7 @@ contract NodeManager {
         aggregationChannel[nextAggregationChannelIndex].storageBytes = storageBytes;
         aggregationChannel[nextAggregationChannelIndex].lifetime = lifetime;
         aggregationChannel[nextAggregationChannelIndex].maxNodes = maxNodes;
-        aggregationChannel[nextAggregationChannelIndex].startTime = block.timestamp;
+        aggregationChannel[nextAggregationChannelIndex].startDate = block.timestamp;
         aggregationChannel[nextAggregationChannelIndex].deposit = deposit;
         aggregationChannelIndexes[msg.sender].push(nextAggregationChannelIndex);
         AggregationChannelCreated(nextAggregationChannelIndex, msg.sender, storageBytes, lifetime, maxNodes, nonce);
@@ -206,13 +210,13 @@ contract NodeManager {
         // aggregation channel should be present
         require(aggregationChannel[aggregationChannelID].owner != address(0));
         // aggregation channel must be alive
-        require((aggregationChannel[aggregationChannelID].startTime +
+        require((aggregationChannel[aggregationChannelID].startDate +
               aggregationChannel[aggregationChannelID].lifetime) > block.timestamp);
         // basic channel must be alive
-        require((basicChannel[basicChannelID].startTime + basicChannel[basicChannelID].lifetime) > block.timestamp);
+        require((basicChannel[basicChannelID].startDate + basicChannel[basicChannelID].lifetime) > block.timestamp);
         // basic channel must expire before aggregation channel
-        require((basicChannel[basicChannelID].startTime + basicChannel[basicChannelID].lifetime) <
-               (aggregationChannel[aggregationChannelID].startTime + aggregationChannel[aggregationChannelID].lifetime));
+        require((basicChannel[basicChannelID].startDate + basicChannel[basicChannelID].lifetime) <
+               (aggregationChannel[aggregationChannelID].startDate + aggregationChannel[aggregationChannelID].lifetime));
         // check that basic channel is not present in the aggregation channel already
         require(!aggregationChannel[aggregationChannelID].basicChannels[basicChannelID]);
         aggregationChannel[aggregationChannelID].basicChannels[basicChannelID] = true;
@@ -271,7 +275,7 @@ contract NodeManager {
 
     function heartbit(uint256 nodeNumber) public {
         require(nodeIndexes[msg.sender][nodeNumber]);
-        uint index = (block.timestamp - startEpoch) / timestampToDate - 11111;
+        uint index = block.timestamp / secondsToDay - 1;
         if (index >= heartbitTotal && index % heartbitUnit == 0) {
             nodes[nodeNumber].heartbits[index % heartbitTotal / heartbitUnit] = 0;
         }
@@ -281,7 +285,7 @@ contract NodeManager {
         if (block.timestamp - nodes[nextNodeIndex].lastRewardDate >= paymentPeriod){
             nodes[nextNodeIndex].lastRewardDate = block.timestamp;
             uint dayToPayFor = 0;
-            for(uint i = 0; i < daysForPayment; i++){
+            for(uint i = 0; i < paymentDays; i++){
                 if (nodes[nodeNumber].heartbits[index % heartbitTotal / heartbitUnit] & (1 * 2 ** (index % heartbitTotal % heartbitUnit)) != 0){
                     dayToPayFor = dayToPayFor + 1;
                     if(i - dayToPayFor > absentDays){
@@ -293,4 +297,10 @@ contract NodeManager {
             tokenAddress.call(bytes4(sha3("transfer(address, uint256)")), msg.sender, dayToPayFor * (dailyMint / getActiveNodesCount()));
         }
     }
+
+    // todo
+    /*
+    function() {
+    throw;
+    }*/
 }
