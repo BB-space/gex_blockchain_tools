e3514165ce027e4f130558269d39db3877c3a337
MKateryna Khotkevych <kkhotkevych@gmail.com>
2018-01-18 13:03:05 +0200

    deploy to server

--- node_manager/deploy/deploy.py
+++ node_manager/deploy/deploy.py
@@ -19,7 +19,7 @@ def write_to_file(**kwargs):
 def deploy():
     print('Deploying contracts')
     project = Project()
-    with project.get_chain(LOCAL_CHAIN_NAME) as chain:
+    with project.get_chain(TEST_CHAIN_NAME) as chain:
         web3 = chain.web3
         owner = web3.eth.accounts[0]
         # GEXToken
@@ -28,20 +28,26 @@ def deploy():
         receipt = check_successful_tx(chain.web3, tx_hash, txn_wait)
         gex_token_address = receipt['contractAddress']
         print('GEXToken deployed')
-        # Contract
-        contract = chain.provider.get_contract_factory('NodeManager')
-        tx_hash = contract.deploy(
+        # NodeManager
+        NodeManager = chain.provider.get_contract_factory('NodeManager')
+        tx_hash = NodeManager.deploy(
             args=[gex_token_address, 5000000],
             transaction={'from': owner}
         )
         receipt = check_successful_tx(chain.web3, tx_hash, txn_wait)
-        contract_address = receipt['contractAddress']
-        print('Contract deployed')
+        node_manager_address = receipt['contractAddress']
+        print('Node Manager deployed')
+        # Ownership
+        gex_token = GEXToken(gex_token_address)
+        tx_hash = gex_token.transact({'from': owner}).transferOwnership(node_manager_address)
+        check_successful_tx(web3, tx_hash, txn_wait)
+        print('GEXToken ownership is established')
+        # Save data
         write_to_file(
             token_abi=GEXToken.abi,
             token_address=gex_token_address,
-            node_manager_abi=contract.abi,
-            node_manager_address=contract_address,
+            node_manager_abi=NodeManager.abi,
+            node_manager_address=node_manager_address,
         )
     print('Deployed')
 
