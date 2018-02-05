77da05cee00fcf58cdfa82b857dd52fb20e8b4ea
Dmitry <jesusinyourtown@gmail.com>
2018-02-05 16:01:52 +0200

    truffle - new deploy function: async, with gex_bot contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,25 +1,32 @@
-var GEXToken = artifacts.require("./GEXToken.sol");
-var NodeManager = artifacts.require("./NodeManager.sol");
+let GEXToken = artifacts.require("./GEXToken.sol");
+let NodeManager = artifacts.require("./NodeManager.sol");
+let GEXBot = artifacts.require("./GexBot.sol");
 
-module.exports = function (deployer) {
-    deployer.deploy(GEXToken).then(function () {
-        deployer.deploy(NodeManager, GEXToken.address, 5000000).then(function () {
-            GEXToken.at(GEXToken.address).transferOwnership(NodeManager.address);
-            var fs = require("fs");
-            var jsonObject = {
-                token_address: GEXToken.address,
-                node_manager_address: NodeManager.address,
-                token_abi: GEXToken.abi,
-                node_manager_abi: NodeManager.abi
-            };
-            fs.writeFile("data.json", JSON.stringify(jsonObject), function (err) {
-                if (err) {
-                    return console.log(err);
-                }
-            });
-       /* }).then( function () {
-             return NodeManager.at(NodeManager.address).getArray();}).then(function(res) {
-                 console.log(res);*/
-        });
-        });
-};
+
+async function deploy(deployer) {
+
+    await deployer.deploy(GEXToken);
+    await deployer.deploy(NodeManager, GEXToken.address, 5000000);
+    GEXToken.at(GEXToken.address).transferOwnership(NodeManager.address);
+
+    await deployer.deploy(GEXBot);
+
+    let fs = require("fs");
+    let jsonObject = {
+        token_address: GEXToken.address,
+        token_abi: GEXToken.abi,
+        node_manager_address: NodeManager.address,
+        node_manager_abi: NodeManager.abi,
+        bot_address: GEXBot.address,
+        bot_abi: GEXBot.abi
+    };
+
+    fs.writeFile("data.json", JSON.stringify(jsonObject), function (err) {
+        if (err) {
+            return console.log(err);
+        }
+    });
+}
+
+
+module.exports = deploy;
